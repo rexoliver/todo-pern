828345dcfa4aed1358ac590c8ea3e1fe957ef3e0
Rex Oliver <rexoliver12@gmail.com>
2023-03-15 16:11:05 -0400

    misc moving

---
+++ client/src/components/InputTodo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/components.css'
+import '../styles/App.css'
+const InputTodo = () => {
+    return (
+        <>
+            <h1 className="apptitle">Pern Todo List</h1>
+        </>
+    );
+};
+
+export default InputTodo;
